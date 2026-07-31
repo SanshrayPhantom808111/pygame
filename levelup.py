0c6b9fcdfcd062fd7576fac9454453876baccf86
SanshrayPhantom808111 <sanshray.pratyush@gmail.com>
2026-07-31 18:28:59 +0530

    have funn

--- levelup.py
+++ levelup.py
@@ -1,7 +1,7 @@
 import pygame
 import random 
-screen_width, screen_height = 500, 400
-movement_speed = 5
+screen_width, screen_height = 1500, 700
+movement_speed = 100
 font_size = 72
 pygame.init()
 background_image = pygame.transform.scale(pygame.image.load("brawlhalla.jpg"),(screen_width, screen_height))
@@ -35,15 +35,15 @@ while running:
         keys = pygame.key.get_pressed()
         x_change = (keys[pygame.K_RIGHT]-keys[pygame.K_LEFT])*movement_speed
         y_change = (keys[pygame.K_DOWN]-keys[pygame.K_UP])*movement_speed
-        sprite1.move = (x_change,y_change)
+        sprite1.move(x_change,y_change)
         if sprite1.rect.colliderect(sprite2.rect):
             all_sprites.remove(sprite2)
             won = True
-    screen.blit(background_image,0,0)
+    screen.blit(background_image,(0,0))
     all_sprites.draw(screen)
     if won:
-        win_text = font.render("You WIN!",True,pygame.Color("black"))
-        screen.blit(win_text,((screen_width-win_text.screen_width())//2,(screen_height - win_text.get_height())//2))
+        win_text = font.render("You WIN!)",True,pygame.Color("black"))
+        screen.blit(win_text,((screen_width-win_text.get_width())//2,(screen_height - win_text.get_height())//2))
     pygame.display.flip()
     clock.tick(90)
 pygame.quit()
